542f06d3f43b074344d46271ca2f41bb477d77b6
Muhammad Abdullah <abdullahpeerzada92@gmail.com>
2026-07-15 05:14:44 +0500

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -10,8 +10,14 @@ export default function Home() {
     <Nav/>
     <Hero/>
     <Trending variant="carousel"/>
+    <div className="flex justify-center items-center">
+    <a href='https://s.click.aliexpress.com/e/_c4MeadQB?bz=725*90' target='_parent'><img width='725' height='90' src='https://ae-pic-a1.aliexpress-media.com/kf/S3da9c6ce045b41f98ae01cacd948f349S.png' /></a>
+    </div>
 <Testimonials variant="grid" />
     <Footer/>
+    <div className="flex justify-center items-center">
+    <a href='https://s.click.aliexpress.com/e/_c3d6i9fv?bz=725*90' target='_parent'><img width='725' height='90' src='https://ae-pic-a1.aliexpress-media.com/kf/S920b0a367808482ba3079918372c02091.png' /></a>
+    </div>
     </div>
   );
 }
